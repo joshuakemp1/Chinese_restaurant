b64d34c22207bdc7c127b5eee25dbd3988d0d468
Joshua Kemp <josh.kemp@ziplist.com>
2014-04-26 21:29:44 -0400

    Generating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChineseRestaurant::Application.config.secret_token = 'a363f3a4cb323e1eb72bcc3c88725223ac45b3f2fdd853769d3dd57014ac9b96503c50b501fa01c945938d802af07b99683ad305f55c49f5e8f458637f3c2f48'
